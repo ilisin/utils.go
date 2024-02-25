e19369dd8f63774ae6b98e6a5cd429b9adfc63c2
ilisin <ilisin.gao@gmail.com>
2024-02-25 21:25:16 +0800

    update

--- httpclient/options.go
+++ httpclient/options.go
@@ -1,44 +1,57 @@
 package httpclient
 
 import (
+	"context"
 	"net/http"
 	"time"
 )
 
-type Option interface {
-	SetOption(client *HttpClient)
-}
-
 type RequestHandler func(r *http.Request) error
 
-type OptionTimeout struct{ timeout time.Duration }
+type Options struct {
+	timeout      time.Duration // request timeout
+	baseURL      string        // base url
+	encoder      Encoder       // request encoder
+	decoder      Decoder       // response decoder
+	handlers     []RequestHandler
+	successCodes []int
+}
 
-func NewTimeoutOption(to time.Duration) Option         { return &OptionTimeout{timeout: to} }
-func (ot *OptionTimeout) SetOption(client *HttpClient) { client.c.Timeout = ot.timeout }
+func newOptions() *Options {
+	return &Options{
+		timeout:  DefaultTimeout,
+		baseURL:  "",
+		encoder:  defaultEncoder,
+		decoder:  defaultDecoder,
+		handlers: []RequestHandler{},
+		successCodes: []int{
+			http.StatusOK,
+		},
+	}
+}
 
-type OptionBaseURL struct{ baseURL string }
+type Option func(*Options)
 
-func NewBaseURLOption(url string) Option               { return &OptionBaseURL{baseURL: url} }
-func (ob *OptionBaseURL) SetOption(client *HttpClient) { client.baseURL = ob.baseURL }
+func WithTimeout(to time.Duration) Option { return func(o *Options) { o.timeout = to } }
+func WithBaseURL(url string) Option       { return func(o *Options) { o.baseURL = url } }
+func WithEncoder(e Encoder) Option        { return func(o *Options) { o.encoder = e } }
+func WithDecoder(d Decoder) Option        { return func(o *Options) { o.decoder = d } }
+func WithRequestHandler(h RequestHandler) Option {
+	return func(o *Options) { o.handlers = append(o.handlers, h) }
+}
+func WithSuccessCodes(codes ...int) Option {
+	return func(o *Options) { o.successCodes = append(o.successCodes, codes...) }
+}
 
-type OptionLog struct{ log bool }
+type contextKeyHeaders struct{}
 
-func NewLogOption(l bool) Option                   { return &OptionLog{log: l} }
-func (lo *OptionLog) SetOption(client *HttpClient) { client.log = lo.log }
+func WithHeaders(ctx context.Context, headers map[string]string) context.Context {
+	return context.WithValue(ctx, contextKeyHeaders{}, headers)
+}
 
-type OptionEncoder struct{ encoder Encoder }
-
-func NewEncoderOption(e Encoder) Option                { return &OptionEncoder{encoder: e} }
-func (oe *OptionEncoder) SetOption(client *HttpClient) { client.requestEncoder = oe.encoder }
-
-type OptionDecoder struct{ decoder Decoder }
-
-func NewDecoderOption(d Decoder) Option                { return &OptionDecoder{decoder: d} }
-func (od *OptionDecoder) SetOption(client *HttpClient) { client.responseDecoder = od.decoder }
-
-type OptionRequestHandler struct{ handler RequestHandler }
-
-func NewRequestHandlerOption(h RequestHandler) Option { return &OptionRequestHandler{handler: h} }
-func (or *OptionRequestHandler) SetOption(client *HttpClient) {
-	client.reqHandlers = append(client.reqHandlers, or.handler)
+func GetHeaders(ctx context.Context) map[string]string {
+	if headers, ok := ctx.Value(contextKeyHeaders{}).(map[string]string); ok {
+		return headers
+	}
+	return nil
 }
